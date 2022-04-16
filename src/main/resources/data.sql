fee2a302ac883e37a7539cdaeb679ef3b89d7599
JuliusKanneh <juliuskanneh7@gmail.com>
2022-04-16 20:49:29 +0200

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO my_users(username, password, enabled)
+    VALUES('user',
+    'pass',
+    true);
+
+INSERT INTO my_users(username, password, enabled)
+    VALUES('admin',
+    'pass',
+    true);
+
+INSERT INTO my_authorities(username, authority)
+    VALUES('user',
+    'ROLE_USER');
+
+INSERT INTO my_authorities(username, authority)
+    VALUES('admin',
+    'ROLE_ADMIN');
